64e211438c12a9f4834d82120818a4728127c631
Lucas de Araujo <araujolucas97@outlook.com>
2018-06-26 13:54:27 +0000

    Add files via upload

---
+++ 3.13/src/pkg3/pkg13/Employee.java
@@ -0,0 +1,35 @@
+package pkg3.pkg13;
+ // @author lucas
+public class Employee {
+    private String nome, sobrenome;
+    private double salario;
+    
+    public Employee(String nome, String sobrenome, double salario){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.salario = salario;
+    }
+
+    Employee() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setSobrenome(String sobrenome){
+        this.sobrenome = sobrenome;
+    }
+    public void setSalario(double salario){
+        this.salario = salario; 
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getSobrenome(){
+        return sobrenome;
+    }
+    public double getSalario(){
+        return salario;
+    }
+}
